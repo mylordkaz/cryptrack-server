a24782c9b8684938acab403cd510c78a34550c8b
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-25 20:57:24 +0900

    feat: register & login endpoint added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import  express  from "express";
 import cors from "cors"
+import router from "./routes/authRoutes";
 
 const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+
+app.use('/api/auth', router)
 
 
 app.listen(PORT, () =>{
