7a40ce819b488b6bf77a28dd5ec8189b18f1447b
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-08 19:45:55 +0900

    feat: logout user logic

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,33 +1,32 @@
 import { Request, Response } from 'express';
 import prisma from '../models/prismaClient';
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import validator from 'validator';
 import dotenv from 'dotenv';
 dotenv.config();
 
-
 const SECRET = process.env.SECRET; //"my_secret"
 
-export const registerUser = async (req:Request, res:Response) => {
+export const registerUser = async (req: Request, res: Response) => {
   const { username, email, password } = req.body;
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
-    if(!email || !password){
-      throw new Error('All fields must be filled')
+    if (!email || !password) {
+      throw new Error('All fields must be filled');
     }
-    if (!validator.isEmail(email)){
-      throw new Error('Email not valid')
+    if (!validator.isEmail(email)) {
+      throw new Error('Email not valid');
     }
-    if(!validator.isStrongPassword(password)){
-      throw new Error('Password not strong enough')
+    if (!validator.isStrongPassword(password)) {
+      throw new Error('Password not strong enough');
     }
 
-    const exist = await prisma.user.findUnique({where: {email}})
-    if(exist){
-      throw new Error('email already in use')
+    const exist = await prisma.user.findUnique({ where: { email } });
+    if (exist) {
+      throw new Error('email already in use');
     }
 
     const user = await prisma.user.create({
@@ -43,7 +42,7 @@ export const registerUser = async (req:Request, res:Response) => {
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
-}
+};
 export const loginUser = async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
@@ -53,12 +52,35 @@ export const loginUser = async (req: Request, res: Response) => {
     return res.status(401).json({ message: 'Invalid credentials' });
   }
 
-  if(!SECRET){
-    console.error("SECRET is not defined in the environment variable")
-    return res.status(500).send('Internal Server Error')
+  if (!SECRET) {
+    console.error('SECRET is not defined in the environment variable');
+    return res.status(500).send('Internal Server Error');
   }
 
-  const token = jwt.sign({ id: user.id, email: user.email }, SECRET, {expiresIn: '3d'});
+  const token = jwt.sign({ id: user.id, email: user.email }, SECRET, {
+    expiresIn: '3d',
+  });
 
   res.json({ token });
+};
+
+export const logoutUser = async (req: Request, res: Response) => {
+  const token = req.header('Authorization')?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401);
+  }
+
+  const isTokenBlacklisted = await prisma.blacklistedToken.findUnique({
+    where: { token },
+  });
+  if (isTokenBlacklisted) {
+    return res.sendStatus(401); //already invalidated
+  }
+
+  // add token to blacklist
+  await prisma.blacklistedToken.create({
+    data: { token },
+  });
+  res.status(200);
 };
