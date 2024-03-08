e432d0f4a64360db7086b66a7f7cfd281511df99
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-08 23:59:22 +0900

    fix: add cors options

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import express from 'express';
-import cors from "cors"
+import cors, { CorsOptions } from 'cors';
 import authRoutes from './routes/authRoutes';
 import userRoutes from './routes/userRoutes';
 
 const app = express();
 const PORT = 3000;
 
+const corsOptions: CorsOptions = {
+  origin: 'http://localhost:5174', // Add your frontend origin(s) here
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
+
 app.use(express.json());
-app.use(cors())
+app.use(cors(corsOptions));
 
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
