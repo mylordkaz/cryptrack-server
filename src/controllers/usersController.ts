7d3e283cefdc45a5f2f25d676eece809a81d4929
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-21 21:14:20 +0900

    feat: add user info update endpoint

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import prisma from '../models/prismaClient';
+import bcrypt from 'bcrypt';
 
 interface AuthenticatedRequest extends Request {
   user?: any;
@@ -20,6 +21,40 @@ export const getUserProfile = async (
     res.json({ id: user.id, username: user.username });
   } catch (error) {
     console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+export const updateUserProfile = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  const userId = req.user.id;
+  const { username, email, currentPassword, newPassword } = req.body;
+
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  if (!(await bcrypt.compare(currentPassword, user.password))) {
+    return res.status(401).json({ message: 'Invalid current password' });
+  }
+
+  try {
+    const newHash = await bcrypt.hash(newPassword, 10);
+
+    await prisma.user.update({
+      where: { id: userId },
+      data: {
+        username: username,
+        email: email,
+        password: newHash,
+      },
+    });
+    res.json({ massage: 'Profile updated succesfully' });
+  } catch (error) {
     res.status(500).send('Internal Server Error');
   }
 };
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-import { getUserProfile } from '../controllers/usersController';
+import {
+  getUserProfile,
+  updateUserProfile,
+} from '../controllers/usersController';
 import { authenticateToken } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
 router.get('/profile', authenticateToken, getUserProfile);
+router.patch('/update', authenticateToken, updateUserProfile);
 
 export default router;
