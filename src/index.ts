516dc59e6c36feecf904029e050c4dac1b51751c
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-03-23 20:41:21 +0900

    typo correction

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,8 @@ const app = express();
 const PORT = process.env.PORT;
 
 const corsOptions: CorsOptions = {
-  origin: 'https://mycryptrack.netlify.app', // 'http://localhost:5173',
+  origin: 'https://mycryptrack.netlify.app',
+  // origin : 'http://localhost:5173',
   credentials: true,
 };
 
