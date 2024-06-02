a70286361f2db7d43ff611ea381bfc9e0e65ebaf
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-06-02 19:43:05 +0900

    Removing the domain option on the cookies, fixed the issue when login-in since I changed, deployment provider

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -47,7 +47,6 @@ export const registerUser = async (req: Request, res: Response) => {
     });
     res.cookie('authToken', token, {
       httpOnly: true,
-      //domain: '.onrender.com',
       secure: process.env.NODE_ENV === 'production',
       path: '/',
       sameSite: 'none',
@@ -79,7 +78,6 @@ export const loginUser = async (req: Request, res: Response) => {
 
   res.cookie('authToken', token, {
     httpOnly: true,
-    // domain: '.onrender.com',
     secure: process.env.NODE_ENV === 'production',
     path: '/',
     sameSite: 'none',
@@ -110,7 +108,6 @@ export const logoutUser = async (req: Request, res: Response) => {
     // remove token from cookie
     res.cookie('authToken', '', {
       httpOnly: true,
-      //domain: '.onrender.com',
       expires: new Date(0),
       secure: process.env.NODE_ENV === 'production',
       path: '/',
