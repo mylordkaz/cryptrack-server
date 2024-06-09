3d1c12593082dc582b8e30fbd7404f3f8d5b66fb
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-06-09 13:39:47 +0900

    add access & refresh token to all the auth func

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -16,7 +16,7 @@ const generateAccessToken = (user: any) => {
 };
 const generateRefreshToken = (user: any) => {
   return jwt.sign({ id: user.id, email: user.email }, REFRESH, {
-    expiresIn: '15m',
+    expiresIn: '7d',
   });
 };
 
@@ -49,22 +49,33 @@ export const registerUser = async (req: Request, res: Response) => {
       },
     });
 
-    if (!SECRET) {
-      console.error('SECRET is not defined in the environment variable');
-      return res.status(500).send('Internal Server Error');
-    }
+    // if (!SECRET) {
+    //   console.error('SECRET is not defined in the environment variable');
+    //   return res.status(500).send('Internal Server Error');
+    // }
 
-    const token = jwt.sign({ id: user.id, email: user.email }, SECRET, {
-      expiresIn: '3d',
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
+
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      path: '/',
+      sameSite: 'none',
     });
-    res.cookie('authToken', token, {
+    res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       path: '/',
       sameSite: 'none',
     });
 
-    res.status(201).json({ id: user.id, username: user.username, token });
+    res.status(201).json({
+      id: user.id,
+      username: user.username,
+      accessToken,
+      refreshToken,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
@@ -82,36 +93,44 @@ export const loginUser = async (req: Request, res: Response) => {
     return res.status(401).json({ message: 'Invalid credentials' });
   }
 
-  if (!SECRET) {
-    console.error('SECRET is not defined in the environment variable');
-    return res.status(500).send('Internal Server Error');
-  }
+  // if (!SECRET) {
+  //   console.error('SECRET is not defined in the environment variable');
+  //   return res.status(500).send('Internal Server Error');
+  // }
 
-  const token = jwt.sign({ id: user.id, email: user.email }, SECRET, {
-    expiresIn: '3d',
-  });
-  console.log('JWT Token:', token);
+  const accessToken = generateAccessToken(user);
+  const refreshToken = generateRefreshToken(user);
 
-  res.cookie('authToken', token, {
+  console.log('access Token:', accessToken);
+  console.log('refresh Token:', refreshToken);
+
+  res.cookie('authToken', accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     path: '/',
     sameSite: 'none',
   });
+  res.cookie('authToken', refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    path: '/',
+    sameSite: 'none',
+  });
+
   console.log('Set-Cookie:', res.getHeaders()['set-cookie']);
-  res.json({ token });
+  res.json({ accessToken, refreshToken });
 };
 
 export const logoutUser = async (req: Request, res: Response) => {
   try {
-    const token = req.cookies.authToken;
+    const refreshToken = req.cookies.refreshToken;
 
-    if (!token) {
+    if (!refreshToken) {
       return res.status(401).send('No token');
     }
 
     const isTokenBlacklisted = await prisma.blacklistedToken.findUnique({
-      where: { token },
+      where: { token: refreshToken },
     });
 
     if (isTokenBlacklisted) {
@@ -120,16 +139,24 @@ export const logoutUser = async (req: Request, res: Response) => {
 
     // add token to blacklist
     await prisma.blacklistedToken.create({
-      data: { token },
+      data: { token: refreshToken },
     });
     // remove token from cookie
-    res.cookie('authToken', '', {
+    res.cookie('accessToken', '', {
       httpOnly: true,
       expires: new Date(0),
       secure: process.env.NODE_ENV === 'production',
       path: '/',
       sameSite: 'none',
     });
+    res.cookie('refreshToken', '', {
+      httpOnly: true,
+      expires: new Date(0),
+      secure: process.env.NODE_ENV === 'production',
+      path: '/',
+      sameSite: 'none',
+    });
+
     res.sendStatus(200); // Logout successful
   } catch (error) {
     console.error('Error during logout:', error);
